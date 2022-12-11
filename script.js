cecdf177eb6101cd782ae29298b3377708298c7c
Behrooz <akbari_behrooz@yahoo.com>
2022-12-12 00:42:24 +0300

    Add function to toggle notification bell on/off

---
+++ script.js
@@ -0,0 +1,5 @@
+const bellOn = document.querySelector(".on");
+
+bellOn.addEventListener('click', () => bellOn.classList.toggle('on'));
+
+// window.addEventListener('click', (e) => console.log(e.target.parentElement))
